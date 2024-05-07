10b1679407ad1263d7ce4550ce14a5390b063471
dipendrashrestha <0xdipendra@gmail.com>
2024-05-07 21:50:51 +0545

    mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import Navbar from "@/scenes/navbar/";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 
 function App() {
 
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setisTopOfPage] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.scrollY == 0){
+        setisTopOfPage(true);
+        setSelectedPage(SelectedPage.Home)
+      }
+      if(window.scrollY !=0){
+        setisTopOfPage(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return ()=>{
+      window.removeEventListener("scroll", handleScroll);
+    }
+  },[])
+
+
   return (
    <div className="app bg-gray-20">
-      <Navbar />
+      <Navbar
+      isTopOfPage={isTopOfPage}
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+      />
    </div>
   )
 }
--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,19 +1,20 @@
+import { SelectedPage } from '@/shared/types';
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 type Props = {
     page: string;
-    selectedPage: string;
-    setSelectedPage : (value: string) => void;
+    selectedPage: SelectedPage;
+    setSelectedPage : (value: SelectedPage) => void;
 }
 
 const Link = ({page,selectedPage, setSelectedPage}: Props) => {
 
-    const lowerCasePage = page.toLowerCase().trim().replace(/ /g,"")
+    const lowerCasePage = page.toLowerCase().trim().replace(/ /g,"") as SelectedPage;
 
   return (
     <AnchorLink
-        className = {`${selectedPage === lowerCasePage} ? "text-primary-500" : ""
+        className = {`${selectedPage === lowerCasePage ? "text-primary-500" : ""} 
             transition duration-500 hover:text-primary-500
         `} 
         href={`#/${lowerCasePage}`}
@@ -23,3 +24,5 @@ const Link = ({page,selectedPage, setSelectedPage}: Props) => {
     </AnchorLink>
   )
 }
+
+export default Link
